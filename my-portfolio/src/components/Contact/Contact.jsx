653d728ee5ab69f52efbb25f44d5425f6ce802cd
NazrawitGemechu <108694052+NazrawitGemechu@users.noreply.github.com>
2025-05-07 18:01:34 +0200

    Update Contact.jsx

--- my-portfolio/src/components/Contact/Contact.jsx
+++ my-portfolio/src/components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/nazrawit-gemechu-828232240/">linkedin.com/NazrawitGemechu</a>
+          <a href="https://www.linkedin.com/in/nazrawit-feyissa-828232240/">linkedin.com/NazrawitGemechu</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
