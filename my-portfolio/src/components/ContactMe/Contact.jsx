68de2566f48fdab1d093186b0c2308afa89aa639
NazrawitGemechu <108694052+NazrawitGemechu@users.noreply.github.com>
2025-05-07 18:12:28 +0200

    Update Contact.jsx

--- my-portfolio/src/components/ContactMe/Contact.jsx
+++ my-portfolio/src/components/ContactMe/Contact.jsx
@@ -62,29 +62,7 @@ export const Contact = () => {
             )}
         </section>
 
-        <footer  className={styles.container}id="contactt">
-      <div className={styles.text}>
-        {/*<h2>Addresess</h2> */}
-        <p>Feel free to reach out!</p>
-      </div>
-      <ul className={styles.links}>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:nazrawitgemechu9706@gmail.com">nazrawitgemechu9706@gmail.com</a>
-        </li>
-        <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="LinkedIn icon"
-          />
-          <a href="https://www.linkedin.com/in/nazrawit-feyissa-828232240/">linkedin.com/NazrawitGemechu</a>
-        </li>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-            <a href="https://github.com/NazrawitGemechu">github.com/NazrawitGemechu</a>
-        </li>
-      </ul>
-    </footer>
+       
     );
 };
 
